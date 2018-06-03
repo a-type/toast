2217e66bc6f73edfb28f926c7c03b736690cb4f4
Grant Forrest <gaforres@gmail.com>
2018-06-03 17:53:50 -0400

    just upgrade

--- ops/scripts/deploy_functions.sh
+++ ops/scripts/deploy_functions.sh
@@ -1,9 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function deploy() {
-  if helm get $IMAGE_NAME --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"; then
-    helm upgrade $IMAGE_NAME ${DIR}/../helm/charts/toast-core --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"
-  else
-    helm install $IMAGE_NAME ${DIR}/../helm/charts/toast-core --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"
-  fi
+  helm upgrade $IMAGE_NAME ${DIR}/../helm/charts/toast-core --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"
+  echo "Release upgraded"
 }
