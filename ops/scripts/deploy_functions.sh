503175e15eeb6447d4f08927588e919ed1742a83
Grant Forrest <gaforres@gmail.com>
2018-06-03 16:06:55 -0400

    set namespace too

--- ops/scripts/deploy_functions.sh
+++ ops/scripts/deploy_functions.sh
@@ -1,7 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function deploy() {
-  if helm get $IMAGE_NAME --tiller-namespace="$TILLER_NAMESPACE"; then
+  if helm get $IMAGE_NAME --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"; then
     helm upgrade $IMAGE_NAME ${DIR}/../helm/charts/toast-core --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"
   else
     helm install $IMAGE_NAME ${DIR}/../helm/charts/toast-core --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"
