f2d6e22a48eb1ee680af8aeac4fe70910f46ab96
Grant Forrest <gaforres@gmail.com>
2018-06-04 18:07:17 -0400

    set image tag to deploy

--- ops/scripts/deploy_functions.sh
+++ ops/scripts/deploy_functions.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function deploy() {
-  helm upgrade ${IMAGE_NAME}${ENV_SUFFIX} ${DIR}/../helm/charts/toast-core --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE"
+  helm upgrade ${IMAGE_NAME}${ENV_SUFFIX} ${DIR}/../helm/charts/toast-core --namespace="$NAMESPACE" --tiller-namespace="$TILLER_NAMESPACE" --set image.tag="$IMAGE_TAG"
   echo "Release upgraded"
 }
