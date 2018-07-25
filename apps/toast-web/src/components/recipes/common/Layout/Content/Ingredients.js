1fb04bd4a4c372de30d3651eda55a0e4bc34764a
Grant Forrest <gaforres@gmail.com>
2018-07-24 21:56:42 -0400

    working jump buttons

--- apps/toast-web/src/components/recipes/common/Layout/Content/Ingredients.js
+++ apps/toast-web/src/components/recipes/common/Layout/Content/Ingredients.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 import contentGridProps from './contentGridProps';
+import { withProps } from 'recompose';
 
-export default styled.div`
+export const ID = 'IngredientsSection';
+
+export default withProps({
+  id: ID,
+  name: ID,
+})(styled.div`
   grid-area: ingredients;
   ${contentGridProps};
-`;
+`);
--- apps/toast-web/src/components/recipes/common/Layout/JumpControls/index.js
+++ apps/toast-web/src/components/recipes/common/Layout/JumpControls/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Container from './Container';
 import Button from './Button';
+import { scroller as scroll } from 'react-scroll';
 
-export default () => (
-  <React.Fragment>
-    <Container>
-      <Button>Ingredients</Button>
-      <Button>Steps</Button>
-    </Container>
-    <div style={{ height: '64px' }} className="spacer" />
-  </React.Fragment>
-);
+export default class JumpControls extends React.PureComponent {
+  jumpToIngredients = () => {
+    scroll.scrollTo('IngredientsSection', {
+      duration: 200,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
+  jumpToSteps = () => {
+    scroll.scrollTo('StepsSection', {
+      duration: 200,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Container>
+          <Button onClick={this.jumpToIngredients}>Ingredients</Button>
+          <Button onClick={this.jumpToSteps}>Steps</Button>
+        </Container>
+        <div style={{ height: '64px' }} className="spacer" />
+      </React.Fragment>
+    );
+  }
+}
