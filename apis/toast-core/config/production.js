01ba5be354b019452624ddc2070d8587102c5e9c
a-type <gforrest@bandwidth.com>
2018-09-06 17:20:28 -0400

    trying to fix prod deployed env

---
+++ apis/toast-core/config/production.js
@@ -0,0 +1,24 @@
+module.exports = {
+  port: process.env.PORT,
+  security: {
+    tokenSecret: process.env.JWT_TOKEN_SECRET,
+    masterEmail: 'toastmaster@toastcooking.app',
+    masterPassword: process.env.TOAST_MASTER_PASSWORD,
+  },
+  database: {
+    neo4j: {
+      endpoint: process.env.NEO4J_BOLT_HOST,
+      user: process.env.NEO4J_USERNAME || 'neo4j',
+      password: process.env.NEO4J_PASSWORD,
+    },
+  },
+  gcloud: {
+    projectId: process.env.GCLOUD_PROJECT_ID,
+    storage: {
+      bucket: process.env.GCLOUD_MEDIA_BUCKET,
+      directories: {
+        images: 'images',
+      },
+    },
+  },
+};
