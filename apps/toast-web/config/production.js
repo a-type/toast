08b58a6f7d7644b3a410a76033e09cb9d7920596
Grant Forrest <gaforres@gmail.com>
2018-09-15 14:55:34 -0400

    patch for prod webpack config

--- apps/toast-web/config/production.js
+++ apps/toast-web/config/production.js
@@ -1,3 +1,7 @@
 module.exports = {
-  origin: 'https://toastcooking.app'
+  origin: 'https://toastcooking.app',
+  auth0: {
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENTID,
+  },
 };
