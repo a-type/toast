dbb993cc0cf84fceecee2cfc2f4787e7d229c7f7
a-type <gforrest@bandwidth.com>
2018-10-18 16:06:58 -0400

    try to fix CI

---
+++ ops/scripts/build_functions.sh
@@ -0,0 +1,30 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function install() {
+  npm ci
+  pushd ${DIR}/../../apis/toast-core
+  npm ci
+  popd
+  pushd ${DIR}/../../apps/toast-web
+  npm ci
+  popd
+}
+
+function test() {
+  pushd ${DIR}/../../apis/toast-core
+  npm test
+  popd
+  pushd ${DIR}/../../apps/toast-web
+  npm test
+  popd
+}
+
+function build() {
+  docker login -u _json_key -p "$(cat ${HOME}/gcloud-service-key.json)" https://us.gcr.io
+  npm run introspect
+  pushd ${DIR}/../../apis/toast-core
+  npm run containerize
+  docker tag toast-core $IMAGE_REPO_SOURCE
+  docker push $IMAGE_REPO_SOURCE
+  popd
+}
