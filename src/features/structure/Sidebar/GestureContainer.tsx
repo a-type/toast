e97149494ec46136f53af623695ef58b4326b0d1
Grant Forrest <gaforres@gmail.com>
2019-04-05 20:19:05 -0400

    fix up menu gestures a bit

--- src/features/structure/Sidebar/GestureContainer.tsx
+++ src/features/structure/Sidebar/GestureContainer.tsx
@@ -21,24 +21,43 @@ export const SidebarGestureContainer: FC<SidebarGestureContainerProps> = ({
     config: { mass: 1, tension: 500, friction: 50 },
     x: naturalPosition,
   }));
-  const bind = useGesture(({ down, x: gestureX, xInitial, args }) => {
-    const isWindow = args[0].element === 'window';
-    const controllingGesture = isWindow
-      ? xInitial < 25 || xInitial - gestureX > 50
-      : true;
+  const bind = useGesture(
+    ({ down, x: gestureX, xInitial, args: [element, isCurrentlyOpen] }) => {
+      const isWindow = element === 'window';
 
-    const xOffset = Math.max(-WIDTH, Math.min(0, gestureX - WIDTH));
+      console.log(gestureX);
 
-    if (controllingGesture) {
+      const wasTapOutside =
+        isCurrentlyOpen &&
+        isWindow &&
+        !down &&
+        gestureX > WIDTH &&
+        xInitial > WIDTH &&
+        Math.abs(gestureX - xInitial) < 15;
+
+      const validGestureStart = isCurrentlyOpen
+        ? xInitial - WIDTH + 25 > 0
+        : xInitial < 25;
+
+      const validGestureMovement = isCurrentlyOpen
+        ? gestureX - xInitial < 25 || (gestureX > WIDTH && xInitial > WIDTH)
+        : gestureX - xInitial > 25;
+
+      if (!validGestureStart || !validGestureMovement) {
+        return;
+      }
+
+      let xOffset = Math.max(-WIDTH, Math.min(0, gestureX - WIDTH));
+
       if (down) {
         set({ x: xOffset });
       } else {
-        const becomeClosed = Math.abs(xOffset + WIDTH) < 25;
+        const becomeClosed = wasTapOutside || Math.abs(xOffset + WIDTH) < 25;
         setOpen(!becomeClosed);
         set({ x: getNaturalPosition(!becomeClosed) });
       }
-    }
-  });
+    },
+  );
 
   useEffect(() => {
     set({ x: getNaturalPosition(open) });
@@ -46,7 +65,7 @@ export const SidebarGestureContainer: FC<SidebarGestureContainerProps> = ({
 
   // bind to window
   useEffect(() => {
-    const cb = bind({ element: 'window' });
+    const cb = bind('window', open);
     window.addEventListener('mousedown', cb.onMouseDown, { passive: true });
     window.addEventListener('touchstart', cb.onTouchStart, { passive: true });
 
@@ -54,11 +73,11 @@ export const SidebarGestureContainer: FC<SidebarGestureContainerProps> = ({
       window.removeEventListener('mousedown', cb.onMouseDown);
       window.removeEventListener('touchstart', cb.onTouchStart);
     };
-  }, []);
+  }, [open]);
 
   return (
     <animated.div
-      {...bind({ element: 'sidebar' })}
+      {...bind('sidebar', open)}
       style={{
         position: 'fixed',
         left: x,
