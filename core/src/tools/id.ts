007d13cab81ae1a6e724416c1ed33830551e267b
Grant Forrest <gaforres@gmail.com>
2019-05-12 18:49:32 -0400

    fix id test

--- core/src/tools/id.ts
+++ core/src/tools/id.ts
@@ -4,11 +4,11 @@ import uuid from 'uuid';
 export default (name?: string) =>
   name
     ? name
-        .slice(0, 24)
         .toLowerCase()
         .split(/\s/)
         .map(s => s.replace(/\W/g, ''))
-        .join('-') +
+        .join('-')
+        .slice(0, 24) +
       '-' +
       shortid.generate()
     : uuid();
