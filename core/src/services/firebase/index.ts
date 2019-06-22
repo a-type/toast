ba2b41099e3943324b79233573600a1cdddd0652
Grant Forrest <gaforres@gmail.com>
2019-06-22 15:27:16 -0400

    set firebase project ID

--- core/src/services/firebase/index.ts
+++ core/src/services/firebase/index.ts
@@ -7,6 +7,7 @@ logger.info('Firebase URL: ', config.firebase.databaseURL);
 firebase.initializeApp({
   //credential: firebase.credential.cert(config.firebase.serviceAccountKeyFile),
   databaseURL: config.firebase.databaseURL,
+  projectId: config.gcloud.projectId,
 });
 
 export default firebase;
