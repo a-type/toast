89fda5abcd0f38b789d94b68e7f45aefea4fd989
Grant Forrest <gaforres@gmail.com>
2019-06-22 18:36:45 -0400

    fix neo4j access for default username

--- planning/src/services/neo4j.ts
+++ planning/src/services/neo4j.ts
@@ -2,8 +2,11 @@ import { v1 as neo4j } from 'neo4j-driver';
 
 const driver = neo4j.driver(
   process.env.NEO4J_BOLT_HOST || 'bolt://localhost:7687',
-  process.env.NEO4J_USERNAME &&
-    neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD),
+  process.env.NEO4J_PASSWORD &&
+    neo4j.auth.basic(
+      process.env.NEO4J_USERNAME || 'neo4j',
+      process.env.NEO4J_PASSWORD,
+    ),
 );
 
 export default driver;
