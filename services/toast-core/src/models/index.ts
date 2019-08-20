495bd40540796950596bb6687efda7160b25b604
Grant Forrest <gaforres@gmail.com>
2019-08-20 08:02:30 -0400

    more cleanup

--- services/toast-core/src/models/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as IngredientCorrection } from './IngredientCorrection';
-export { default as GroupInvitation } from './GroupInvitation';
-export { default as PurchaseList } from './PurchaseList';
